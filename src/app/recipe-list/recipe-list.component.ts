d3a13b8c77e1d6874fd6a19862b5cba6f577f75a
Yinon Bar <yinonbar1988@gmail.com>
2023-04-20 23:10:10 +0300

    header

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  public recipes: Recipe[];
 }
