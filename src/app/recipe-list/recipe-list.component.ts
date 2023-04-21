89c74e2b11475c571982b85db201b3423234b7ff
Yinon Bar <yinonbar1988@gmail.com>
2023-04-21 11:53:05 +0300

    header

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -7,5 +7,11 @@ import { Recipe } from '../models/recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-  public recipes: Recipe[];
+  public recipes: Recipe[] = [
+    new Recipe(
+      'Test recipe',
+      'bla bla bla',
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fstephaniessweets.com%2Fmoist-triple-chocolate-cake%2F&psig=AOvVaw2uk0oh1wDoZJ7GJ8pSRnnz&ust=1682107987981000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPivpNuiuf4CFQAAAAAdAAAAABAE'
+    ),
+  ];
 }
