d59f1c18cdc4b4d218a786faa358699b4d3726a1
Yinon Bar <yinonbar1988@gmail.com>
2023-04-21 15:34:51 +0300

    start over with bootstrap 3

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-  public ingredients = [];
+  public ingredients: Ingredient[] = [
+    new Ingredient('Apples', 4),
+    new Ingredient('Oranges', 2),
+    new Ingredient('Sugar', 10),
+  ];
 }
